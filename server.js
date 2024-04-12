6ac5945b6d2197cb9da0e19779b37a06e672dc24
DannyPirouz <dannypirouz@gmail.com>
2024-04-11 21:59:36 -0700

    deleted unnecessary PORT key

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/cities', async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
